05ec972bf45aa0b5ff42df5a0ea8d79b633992ea
Dhanushka Samarakoon <99551588+DsamarakooN@users.noreply.github.com>
2023-01-19 15:55:54 +0530

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const [age, setAge] = useState(0);
+
+  const increasAge = () => {
+    setAge(age + 1);
+  };
+
+  const decreaseAge = () => {
+    setAge(age - 1);
+  };
+
+  const setToZero = () => {
+    setAge(0);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{age}</h1> <br/>
+      <button onClick={increasAge}>Increase Age</button>
+      <button onClick={decreaseAge}>Dicrease Age</button>
+      <button onClick={setToZero}>Set to Zero</button>
     </div>
   );
 }
 
 export default App;
+
